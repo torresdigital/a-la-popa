0c09617b4f98a22de6a3bf9351403c76b1f32e74
Anderson Torres <atorresnh@gmail.com>
2024-04-03 02:33:24 -0300

    Fixes

--- Sources/Client/ClientUI.cpp
+++ Sources/Client/ClientUI.cpp
@@ -34,9 +34,9 @@ DEFINE_SPADES_SETTING(_sup_instant_P, "PUT YOU instant mensage p key here /_sup_
 DEFINE_SPADES_SETTING(_sup_instant_MS4, "Mouse Button 4 /_sup_instant_MS4 ");
 DEFINE_SPADES_SETTING(_sup_instant_MS5, "Mouse Button 5 /_sup_instant_MS5 ");
 
-// SOPA !
-DEFINE_SPADES_SETTING(sopa, "SOPA BRV's, are so delicious ♨");
-// LA POPA !
+// amogus
+DEFINE_SPADES_SETTING(amogus, "SOPA BRV's, are so delicious ♨");
+// sussy
 
 namespace spades {
 	namespace client {
@@ -104,7 +104,7 @@ void ClientUI::sendMS5() {
 void ClientUI::susamogus() { // red sus
 	if (!client) // real life amogus
 		return; // sussy
-	client->net->SendChat((std::string)sopa, false); // amogus
+	client->net->SendChat((std::string)amogus, false); // amogus
 }
 
 // macro shit stuff thing end
